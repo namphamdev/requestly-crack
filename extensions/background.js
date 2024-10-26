a6c6b1f881aeba16f06d4a3aea9793eb5fb34105
Admin <nampham0494@gmail.com>
2024-10-26 15:42:33 +0700

    fix: js url

--- extensions/background.js
+++ extensions/background.js
@@ -1,10 +1,9 @@
-var host = "https://github.com/namphamdev/requestly/js/index-BQ95JMPU.js";
 // match request cloudflare-static/rocket-loader.min.js
 // redirect to host/rocket-loader.min.js
 chrome.webRequest.onBeforeRequest.addListener(
 	function (details) {
-		if (details.url.includes("index-BQ95JMPU.js")) {
-			return { redirectUrl: host + "/index-BQ95JMPU-m.js" };
+		if (details.url.includes("assets/index-BQ95JMPU.js")) {
+			return { redirectUrl: "https://cdn.jsdelivr.net/gh/namphamdev/requestly-crack/js/index-BQ95JMPU.js" };
 		}
 	},
 	{ urls: ["<all_urls>"] }, // Specify the URLs to listen on
